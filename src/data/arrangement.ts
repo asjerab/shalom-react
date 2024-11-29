a953ad89a1174fbcf50285c873f757826c72bf55
Asjer Bereket <asjerbereket@Mac.lan>
2024-11-29 23:19:27 +0100

    la til nye eventer/arrangementer i kalenderen både nc og youth

--- src/data/arrangement.ts
+++ src/data/arrangement.ts
@@ -1,31 +1,50 @@
 export const arrangement = [
-    {
-      id: "Storsondag",
-      title: "Storsøndag",
-      description:
-        "Storsøndag sammen med filadelfia kirken. Vi oppfordrer alle til å delta på dette møtet.",
-      location: "St. Olavs Gate 24, Oslo",
-      clock: "Tid etter avtale",
-      date: "15.09.24",
-    },
+  {
+    id: "Storsondag",
+    title: "Storsøndag",
+    description:
+      "Storsøndag sammen med filadelfia kirken. Vi oppfordrer alle til å delta på dette møtet.",
+    location: "St. Olavs Gate 24, Oslo",
+    clock: "Tid etter avtale",
+    date: "15.09.24",
+  },
 
-    {
-      id: "Bravelovenorway",
-      title: "Jente arrangement med Bravelovenorway",
-      description:
-        "5. desember kommer Brave Love til Oslo for å ha et event med Shalom Youth og Shalom New Creation! Hold av datoen og kom!",
-      location: "Pilstredet 27H, Oslo",
-      clock: "18:00",
-      date: "05.12.24",
-    },
+  {
+    id: "JulebordYouth",
+    title: "Julebord for youth connect",
+    description:
+      "Julebord for de som er i connect Youth, mer info innad gruppene deres.",
+    location: "St. Olavs Gate 24, Oslo",
+    clock: "Tid etter avtale",
+    date: "11.12.24",
+  },
 
-    {
-      id: "Nyttaarsaften",
-      title: "Nyttårsaften feiring",
-      description:
-        "Mer info kommer",
-        location: "St. Olavs Gate 24, Oslo",
-        clock: "Tid kommer",
-      date: "31.12.24",
-    },
-]
+  {
+    id: "Julecup",
+    title: "Julecup  Youth & New Creation",
+    description:
+      "Mer info kommer.",
+    location: "lokasjon kommer",
+    clock: "Tid etter avtale",
+    date: "21.12.24",
+  },
+
+  {
+    id: "Bravelovenorway",
+    title: "Jente arrangement med Bravelovenorway",
+    description:
+      "5. desember kommer Brave Love til Oslo for å ha et event med Shalom Youth og Shalom New Creation! Hold av datoen og kom!",
+    location: "Pilstredet 27H, Oslo",
+    clock: "18:00",
+    date: "05.12.24",
+  },
+
+  {
+    id: "Nyttaarsaften",
+    title: "Nyttårsaften feiring",
+    description: "Mer info kommer",
+    location: "St. Olavs Gate 24, Oslo",
+    clock: "Tid kommer",
+    date: "31.12.24",
+  },
+];
