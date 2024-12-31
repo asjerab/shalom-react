be102297832af815f1e1dc17ac4e304f195f5a08
asjerab <asjer.contact@gmail.com>
2024-12-31 13:48:59 +0100

    fiksa formatering

--- src/app/components/newYearsCountDown.tsx
+++ src/app/components/newYearsCountDown.tsx
@@ -7,7 +7,7 @@ const NewYearsCountdown = () => {
   useEffect(() => {
     const calculateTimeRemaining = () => {
       const now = new Date();
-      const newYear = new Date(2025, 0, 1, 0, 0, 0); 
+      const newYear = new Date(2025, 0 , 1, 0, 0, 0); 
       const timeDifference = newYear.getTime() - now.getTime();
 
       if (timeDifference <= 0) {
@@ -16,12 +16,11 @@ const NewYearsCountdown = () => {
         return;
       }
 
-      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
       const hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24);
       const minutes = Math.floor((timeDifference / (1000 * 60)) % 60);
       const seconds = Math.floor((timeDifference / 1000) % 60);
 
-      setTimeRemaining(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+      setTimeRemaining(`${hours}h ${minutes}m ${seconds}s`);
     };
 
     const intervalId = setInterval(calculateTimeRemaining, 1000);
@@ -32,7 +31,7 @@ const NewYearsCountdown = () => {
     <>
       <main className="flex justify-center items-center pt-[150px] p-[30px] h-screen">
         <div className="flex flex-col text-center primaryFontRegular text-slate-50" style={{fontSize: "clamp(20px, 10vw, 125px)"}}>
-            {isVisible && <h1 className="leading-15">2024 slutter om</h1>}
+            {isVisible && <h1 className="leading-15 primaryFontRegular">2024 slutter om</h1>}
             <h1 className="bg-[#1b1b1b] rounded-full">{timeRemaining}</h1>
             </div>
       </main>
