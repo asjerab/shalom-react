bac468e9c1ef0bc932527b693f9b30714bc0450c
danielteckle1212 <danieltt@ifi.uio.no>
2025-05-23 14:05:37 +0200

    typos

--- src/app/components/NcKalender.tsx
+++ src/app/components/NcKalender.tsx
@@ -134,7 +134,7 @@ const Home: React.FC = () => {
             className="primaryFontRegular text-slate-50 w-full max-w-[680px]"
             style={{ fontSize: "clamp(15px, 3.5vw, 25px)" }}
           >
-            Dette er Shalom youth sin helt egne kalender. Her kan du se hva
+            Dette er Shalom New creation sin helt egne kalender. Her kan du se hva
             som skjer i løpet av måneden du har valgt. I tillegg ser du datoene
             for de ulike arrangementene samt dagen det skal være på.
           </p>
@@ -146,3 +146,4 @@ const Home: React.FC = () => {
 };
 
 export default Home;
+
