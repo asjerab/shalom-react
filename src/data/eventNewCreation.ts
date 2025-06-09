876a0253f78ad9584de8e20559d927a8e76d8417
asjerab <asjer.contact@gmail.com>
2025-06-09 10:12:19 +0200

    la til info om sommeravslutningen

--- src/data/eventNewCreation.ts
+++ src/data/eventNewCreation.ts
@@ -205,11 +205,11 @@ export const eventsNc = [
   },
   {
     id: "Sommeravslutning",
-    title: "Sommer avslutning!",
+    title: "Sommerfest!",
     description:
-      "Mer info kommer",
-    location: "Lokasjon kommer",
-    clock: "Tid kommer",
+      "Det skal være aktiviteter, salg av grillmat og utdeling av «diplom» for de som har fullført vgs, universitetet, fått fagbrev osv. Det finner mer info om dette på lenken https://forms.gle/yEqAdMbiywoD9jENA",
+    location: "Ekebergsletta",
+    clock: "14:00",
     date: "14.05.25",
   },
 ];
